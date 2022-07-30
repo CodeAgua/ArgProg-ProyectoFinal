776544e3674b02ae4e6239fcb3acb89b1a574a3c
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2022-07-29 22:43:39 -0300

    Rest of the body created.

--- ProyectoFinal_ArgProg/src/app/app.module.ts
+++ ProyectoFinal_ArgProg/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LoginscreenComponent } from './components/loginscreen/loginscreen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    LoginscreenComponent
   ],
   imports: [
     BrowserModule,
