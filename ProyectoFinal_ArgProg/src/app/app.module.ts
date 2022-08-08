b2686cf94e4ae8da6df0dacd5ff011d680743a8b
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2022-08-08 01:29:36 -0300

    WIP Service for login authentication

--- ProyectoFinal_ArgProg/src/app/app.module.ts
+++ ProyectoFinal_ArgProg/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -34,6 +34,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
 
     NgCircleProgressModule.forRoot({
